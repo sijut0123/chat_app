8e3af3e031ba5723d66195d4851021731ab17ed3
榎橘征爾 <kakitsuseiji@enokitachibanaseijinoMacBook-Air.local>
2024-07-09 18:36:16 +0900

    時間の追加

--- main.mjs
+++ main.mjs
@@ -11,9 +11,12 @@ const prisma = new PrismaClient();
 const template = fs.readFileSync("./template.html", "utf-8");
 app.get("/", async (request, response) => {
   const posts = await prisma.post.findMany();
+  const dateFormatter = new Intl.DateTimeFormat('ja-JP', {
+    hour: '2-digit', minute: '2-digit', hour12: false
+  });
   const html = template.replace(
     "<!-- posts -->",
-    posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join(""),
+    posts.map((post) => `<li>${escapeHTML(post.message)} ${dateFormatter.format(new Date(post.created_at))}</li>`).join(""),
   );
   response.send(html);
 });
